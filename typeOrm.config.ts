ffccbffd5a67ea7e60a627436b9831c7568f18a3
Saim Saudagar <saudagarsaim@gmail.com>
2025-09-11 20:21:06 +0500

    Refactor TypeORM configuration to use individual database connection parameters instead of DATABASE_URL. Updated typeOrm.config.ts to specify host, port, username, password, and database for improved clarity and maintainability.

--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -8,12 +8,13 @@ const configService = new ConfigService();
 
 export default new DataSource({
   type: 'postgres',
-  url: configService.get('DATABASE_URL'),
+  host: configService.get('DB_HOST_NAME'),
+  port: parseInt(configService.get('DB_PORT') || '5432'),
+  username: configService.get('DB_USER'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
   entities: ['src/**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*{.ts,.js}'],
   synchronize: false,
   logging: true,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
