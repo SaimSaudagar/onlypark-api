9c3308c7722a46e95683aabd54e0907ed8ed68ad
Saim Saudagar <saudagarsaim@gmail.com>
2025-09-11 20:17:38 +0500

    Refactor SSL configuration in TypeORM setup to enhance security and maintainability. Updated typeOrm.config.ts to enable SSL and encapsulate rejectUnauthorized setting within an extra property.

--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -17,7 +17,10 @@ export default new DataSource({
   migrations: ['src/migrations/*{.ts,.js}'],
   synchronize: false,
   logging: true,
-  ssl: {
-    rejectUnauthorized: false,
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
